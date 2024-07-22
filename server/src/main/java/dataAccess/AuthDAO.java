f52c50f219d0b6818a92fc86d4ce5a2721304c05
Madsen Evans <xcmaddog@gmail.com>
2024-07-22 10:19:52 -0600

    AuthDAO interface

---
+++ server/src/main/java/dataAccess/AuthDAO.java
@@ -0,0 +1,10 @@
+package dataAccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    public void createAuth(AuthData authData);
+    public AuthData getAuth(String authToken);
+    public void deleteAuth(String authToken);
+    public void clear();
+}
