d3430983130212e92c6f113841b85ece04b131a0
Madsen Evans <xcmaddog@gmail.com>
2024-07-22 11:23:26 -0600

    ListGamesRequest

---
+++ server/src/main/java/request/ListGamesRequest.java
@@ -0,0 +1,29 @@
+package request;
+
+import java.util.Objects;
+
+public class ListGamesRequest {
+
+    private final String authToken;
+
+    public ListGamesRequest(String authToken){
+        this.authToken = authToken;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListGamesRequest that = (ListGamesRequest) o;
+        return Objects.equals(authToken, that.authToken);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(authToken);
+    }
+}
