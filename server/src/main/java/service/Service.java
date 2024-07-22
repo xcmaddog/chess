6d5e24eb35f543592c2b9ec6f4d8900d5347db5e
Madsen Evans <xcmaddog@gmail.com>
2024-07-22 11:54:55 -0600

    Service superclass

---
+++ server/src/main/java/service/Service.java
@@ -0,0 +1,23 @@
+package service;
+
+import dataAccess.AuthDAO;
+import dataAccess.GameDAO;
+import dataAccess.UserDAO;
+import model.AuthData;
+
+public class Service {
+
+    private final UserDAO userDAO;
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public Service (UserDAO userDAO, GameDAO gameDAO, AuthDAO authDAO){
+        this.userDAO = userDAO;
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public boolean isAuthorized(String authToken){
+        return authDAO.getAuth(authToken) != null;
+    }
+}
