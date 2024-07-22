4f7c7f380db91e2672882aed4ed3ff5e880b3ee7
Madsen Evans <xcmaddog@gmail.com>
2024-07-22 11:28:49 -0600

    CreateGameRequest

---
+++ server/src/main/java/request/CreateGameRequest.java
@@ -0,0 +1,35 @@
+package request;
+
+import java.util.Objects;
+
+public class CreateGameRequest {
+
+    private final String authToken;
+    private final String gameName;
+
+    public CreateGameRequest (String authToken, String gameName){
+        this.authToken = authToken;
+        this.gameName = gameName;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CreateGameRequest that = (CreateGameRequest) o;
+        return Objects.equals(authToken, that.authToken) && Objects.equals(gameName, that.gameName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authToken, gameName);
+    }
+}
