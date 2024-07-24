d816e562a950771d8c184a9c12eec6174e879e04
Madsen Evans <xcmaddog@gmail.com>
2024-07-23 20:59:08 -0600

    Cleaning Code (3)

--- server/src/test/java/service/GamesServiceTest.java
+++ server/src/test/java/service/GamesServiceTest.java
@@ -11,7 +11,6 @@ import model.AuthData;
 import model.GameData;
 import model.GameInfo;
 import model.UserData;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import request.CreateGameRequest;
@@ -19,7 +18,6 @@ import request.JoinGameRequest;
 import request.ListGamesRequest;
 import result.CreateGameResult;
 import result.JoinGameResult;
-import result.ListGamesResult;
 
 import java.util.HashSet;
 
@@ -47,7 +45,8 @@ class GamesServiceTest {
         AuthData firstAuth = new AuthData(firstAuthToken,"JoeBob");
         memoryAuthDAO.createAuth(firstAuth);
 
-        UserData secondUser = new UserData("JillSmith", "ConfidentPassword", "jills2002@yahoo.com");
+        UserData secondUser = new UserData("JillSmith", "ConfidentPassword",
+                "jills2002@yahoo.com");
         memoryUserDAO.createUser(secondUser);
         String secondAuthToken = "thisIsTheSecondAuthToken";
         AuthData secondAuth = new AuthData(secondAuthToken, "JillSmith");
@@ -56,8 +55,10 @@ class GamesServiceTest {
         ChessGame withAMove = new ChessGame();
         ChessMove theMove = new ChessMove(new ChessPosition(2,3),new ChessPosition(3,3));
         withAMove.makeMove(theMove);
-        thisGame = new GameData(4321,"Hippocampus",withAMove,"JillSmith","JoeBob");
-        oneGame = new GameData(1234,"TheGame", new ChessGame(), null, "JillSmith");
+        thisGame = new GameData(4321,"Hippocampus",withAMove,"JillSmith",
+                "JoeBob");
+        oneGame = new GameData(1234,"TheGame", new ChessGame(), null,
+                "JillSmith");
         memoryGameDAO.createGame(thisGame);
         memoryGameDAO.createGame(oneGame);
 
@@ -65,49 +66,63 @@ class GamesServiceTest {
     }
 
     @Test
-    void getGames() throws InvalidMoveException, dataaccess.DataAccessException {
-
+    void getGamesPositive() throws InvalidMoveException, dataaccess.DataAccessException {
         HashSet<GameInfo> expected = new HashSet<GameInfo>();
         expected.add(new GameInfo(thisGame));
         expected.add(new GameInfo(oneGame));
 
         ListGamesRequest listGamesRequest = new ListGamesRequest("thisIsTheFirstAuthToken");
-        assertEquals(expected, gameService.getGames(listGamesRequest).getAllGameInfo()); // should have a hash set of games
-
-        listGamesRequest = new ListGamesRequest("thisIsAnInvalidAuthToken");
-        ListGamesRequest finalListGamesRequest = listGamesRequest;
-        assertThrows(dataaccess.DataAccessException.class,() -> gameService.getGames(finalListGamesRequest)); //throws for invalid authToken
-
+        // should have a hash set of games
+        assertEquals(expected, gameService.getGames(listGamesRequest).getAllGameInfo());
     }
 
     @Test
-    void createGame() throws InvalidMoveException, dataaccess.DataAccessException {
+    void getGamesNegative() throws InvalidMoveException, dataaccess.DataAccessException {
+        HashSet<GameInfo> expected = new HashSet<GameInfo>();
+        expected.add(new GameInfo(thisGame));
+        expected.add(new GameInfo(oneGame));
+
+        ListGamesRequest listGamesRequest = new ListGamesRequest("thisIsAnInvalidAuthToken");
+        ListGamesRequest finalListGamesRequest = listGamesRequest;
+        //throws for invalid authToken
+        assertThrows(dataaccess.DataAccessException.class,() -> gameService.getGames(finalListGamesRequest));
+    }
+
+    @Test
+    void createGamePositive() throws InvalidMoveException, dataaccess.DataAccessException {
         String someAuth = "thisIsTheFirstAuthToken";
         CreateGameRequest createGameRequest = new CreateGameRequest("TheGame");
         CreateGameResult expected = new CreateGameResult(1);
         assertEquals(expected, gameService.createGame(someAuth, createGameRequest));
-
-        String invalidAuth = "anInvalidAuthToken";
-        createGameRequest = new CreateGameRequest("Bogus");
-        CreateGameRequest finalCreateGameRequest = createGameRequest;
-        assertThrows(dataaccess.DataAccessException.class, () -> gameService.createGame(invalidAuth, finalCreateGameRequest));
     }
 
     @Test
-    void joinGame() throws InvalidMoveException, dataaccess.DataAccessException {
+    void createGameNegative() throws InvalidMoveException, dataaccess.DataAccessException {
+        String invalidAuth = "anInvalidAuthToken";
+        CreateGameRequest createGameRequest = new CreateGameRequest("Bogus");
+        CreateGameRequest finalCreateGameRequest = createGameRequest;
+        assertThrows(dataaccess.DataAccessException.class, () -> gameService.createGame(invalidAuth,
+                finalCreateGameRequest));
+    }
+
+    @Test
+    void joinGamePositive() throws InvalidMoveException, dataaccess.DataAccessException {
         String anAuthToken = "thisIsTheFirstAuthToken";
         JoinGameRequest joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.WHITE, 1234);
         JoinGameResult expected = new JoinGameResult();
         assertEquals(expected, gameService.joinGame(anAuthToken, joinGameRequest)); //successful add to a game
-
+    }
+    void joinGameNegative() throws InvalidMoveException, dataaccess.DataAccessException {
         String invalidToken = "anInvalidAuthToken";
-        joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.BLACK, 1234);
+        JoinGameRequest joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.BLACK, 1234);
         JoinGameRequest finalJoinGameRequest = joinGameRequest;
-        assertThrows(dataaccess.DataAccessException.class, () -> gameService.joinGame(invalidToken, finalJoinGameRequest));// invalid AuthToken
+        assertThrows(dataaccess.DataAccessException.class, () -> gameService.joinGame(invalidToken,
+                finalJoinGameRequest));// invalid AuthToken
 
         String anotherAuth = "thisIsTheSecondAuthToken";
         joinGameRequest = new JoinGameRequest(ChessGame.TeamColor.WHITE, 1234);
         JoinGameRequest finalJoinGameRequest1 = joinGameRequest;
-        assertThrows(dataaccess.DataAccessException.class, () -> gameService.joinGame(anotherAuth, finalJoinGameRequest1)); //Someone is already White in this game
+        assertThrows(dataaccess.DataAccessException.class, () -> gameService.joinGame(anotherAuth,
+                finalJoinGameRequest1)); //Someone is already White in this game
     }
 }
